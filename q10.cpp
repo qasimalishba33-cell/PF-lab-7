1b45a4bc9c97bbf5c9260383130025efc72b1db8
Alishba Qasim <qasimalishba33@gmail.com>
2026-01-04 12:12:29 +0500

    Pattern Printing using nested loops added

---
+++ q10.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	int n ;
+	cout<<"Enter the number:"<<endl;
+	cin>>n;
+	
+	for (int i = 1; i <= n; i++){
+	
+		for (int spaces = n-i; spaces>=1; spaces--){
+		
+		cout<<" ";
+		
+		}
+		
+		for (int j = 1; j <= (2*i)-1 ; j++){
+		cout<<j;
+		}
+		
+		cout<<endl;
+		}
+		
+		
+	
+	
+	}
